b05670f04ef60ac034c28087ccd9f79fcc0b4269
Robert Bubnik <rbubnik@gmail.com>
2023-12-20 17:00:04 -0500

    passing all tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 0
+    y = 10
+    while y > i:
+        print(y)
+        y -= 1
+    
+    print('Happy New Year!')
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squares = [number*number for number in int_list]
+    return squares
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if (i/3).is_integer() == True and (i/5).is_integer() == True :
+            print('FizzBuzz')
+            i += 1
+        elif (i/3).is_integer() == True:
+            print('Fizz')
+            i += 1
+        elif (i/5).is_integer() == True:
+            print('Buzz')
+            i += 1
+        
+        else:
+            print(i)
+            i += 1
+
+
+
